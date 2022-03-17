04552f9ab03d907543d75cd3e95b42406c0693b4
Chris Meyer <88792515+Logs4Days@users.noreply.github.com>
2022-03-17 02:14:49 -0400

    Cleaned up directories

--- scan.sh
+++ scan.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Gather list of alive hosts
-sudo nmap -n -sn -iL targets -oG - | awk '/Up$/{print $2}' > aliveHosts
+sudo nmap -n -sn -iL targets -oG - | awk '/Up$/{print $2}' > Reports/aliveHosts
+
+# Heavy port scan 
+sudo nmap -n -p 1-9999 -iL Reports/aliveHosts > Reports/openPorts
+
 
 # Assess vulns 
-sudo nmap -n -sV --script vulners --script-args mincvss=5.0 -iL aliveHosts > vulners.txt
+sudo nmap -n -sV --script vulners --script-args mincvss=5.0 -iL Reports/aliveHosts > Reports/vulners
