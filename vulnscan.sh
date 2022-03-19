407e9a96dfeed7d37f43b648d95059064568dfe3
Chris Meyer <88792515+Logs4Days@users.noreply.github.com>
2022-03-19 10:56:07 -0400

    Update vulnscan.sh
    Added min CVE score - anything under 5 is a lot of noise for reporting

--- vulnscan.sh
+++ vulnscan.sh
@@ -4,5 +4,5 @@ mkdir Reports
 
 for i in `seq 1 10`;
 do
-  sudo nmap -n -sV --script vulners -iL AliveHosts/team${i} > Reports/team${i}
+  sudo nmap -n -sV --script vulners --script-args mincvss=5.0 -iL AliveHosts/team${i} > Reports/team${i}
 done
